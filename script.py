71f15a163fcc47f4a28862a27475d4f49d1e5245
Faisal <faysal.ahmed@outlook.com.au>
2020-08-25 00:10:17 +1000

    added some code

---
+++ script.py
@@ -0,0 +1,20 @@
+# python -m venv name
+# To create the envoirnment
+# Control Space short cut for Json Settings
+# git and vs code
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greeting(who_to_greet):
+    return f"hello{who_to_greet}"
+
+
+r = requests.get("https://www.youtube.com/watch?v=-nh9rCzPJ20")
+print(r.status_code)
